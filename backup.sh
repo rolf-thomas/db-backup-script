2c9080c14848095dea33abde0cba6d22e7e2eb5b
Rolf-Thomas Langer <rtl@idee-x.de>
2016-08-16 12:53:32 +0200

    Initial commit

---
+++ backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# inspired by https://github.com/webstylr/simple-mysqlbackup/blob/master/mysqlbackup.sh
+#
+# Requirements
+# ------------
+# /root/.my.cnf:
+# --
+# [mysqldump]
+# user=mysql-backup-user
+# password=mysql-backup-user-password
+# --
+# Important: chmod 0600 /root/.my.cnf
+#
+# Check Option --default-character-set
+#
+# Tip: create a seperate mysql backup user
+# CREATE USER 'backup'@'localhost' IDENTIFIED BY '[password]';
+# GRANT SELECT, LOCK TABLES ON *.* TO 'backup'@'localhost';
+# FLUSH PRIVILEGES;
+
+### Write log to temporary file  ###
+# exec &> /tmp/backup.log
+
+JOBNAME="[projectname] ";
+STORAGEDIR="/var/www/[…]/backup";
+DATE=`date +"%Y-%m-%d"`;
+TIME=`date +"%H-%M"`;
+DBNAME="[dbname]";
+BACKUPCOMMAND="mysqldump --default-character-set=utf8mb4 ${DBNAME} | gzip > ${STORAGEDIR}/backup-db-${DBNAME}-${DATE}--${TIME}.gz";
+
+echo "\n";
+echo "$DATE $TIME - start db backup '$JOBNAME'";
+echo $BACKUPCOMMAND;
+eval $BACKUPCOMMAND;
+echo "$DATE $TIME - end db backup '$JOBNAME'";
